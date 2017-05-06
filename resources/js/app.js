40137cea8541fd5b95bc2b12e7b7ddf050d73174
Kazuki_Kamizuru <k.amago0721@gmail.com>
2017-05-06 16:34:40 +0900

    fixes #226 jQueryの依存をなくす

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,85 +1,51 @@
 require('./bootstrap');
 
+import Dialog from './components/Dialog.vue';
+import Instruction from './components/Instruction.vue';
+import Quantity from './components/Quantity.vue';
+
+// new Vue({
+//     delimiters: ['${', '}'],
+//     data: {
+//         title: '',
+//         message: '',
+//     },
+//     components: {
+//         dialog: Dialog,
+//     },
+// }).$mount('.container');
+
 new Vue({
     delimiters: ['${', '}'],
     data: {
         id: '',
         cuisine: {},
+        title: '',
+        message: '',
     },
     components: {
-        instructions: {
-            props: ['instructions'],
-            template: `
-                <ul class="cookings">
-                    <li v-for="instruction in instructions">
-                        <span class="cooking-order" v-text="getSortOrder(instruction)"></span>：
-                        <input type="text" name="instructions.description" v-model="instruction.description" class="cooking-description">
-                    </li>
-                </ul>
-            `,
-            methods: {
-                getSortOrder(_instruction) {
-                    if (!_instruction.sort_order) {
-                        _instruction.sort_order = this.instructions.length;
-                    }
-                    return _instruction.sort_order;
-                },
-            },
-        },
-        quantities: {
-            props: ['quantities'],
-            data() {
-                return {
-                    classifications: [
-                        '野菜',
-                        '肉',
-                        '調味料',
-                        '魚',
-                    ],
-                };
-            },
-            template: `
-                <ul class="quantities">
-                    <li v-for="quantity in quantities">
-                        <input type="text" name="quantities.foodstuff.name" v-model="getFoodstuffName(quantity)">
-                        <input type="text" name="quantities.detail" v-model="quantity.detail">
-                        <select name="quantities.foodstuff.classification" v-model="quantity.foodstuff.classification">
-                            <option v-for="classification in classifications" v-text="classification"></option>
-                        </select>
-                    </li>
-                </ul>
-            `,
-            methods: {                
-                getFoodstuffName(_quantity) {
-                    return _quantity.foodstuff ? _quantity.foodstuff.name : '';
-                },
-            },
-        }
+        notice: Dialog,
+        instructions: Instruction,
+        quantities: Quantity,
     },
     methods: {
-        toTop: function() {
+        toTop() {
             location.href = '/';
         },
-        toMenu: function() {
+        toMenu() {
             location.href = '/recipe/menu/';
         },
-        addRow(_key) {
-            this.cuisine[_key].push({});
-        },
-        deleteRow(_key) {
-            this.cuisine[_key].pop();
-        },
         save() {
-            console.log('ポストされたぜ！！！');
-            console.log(JSON.stringify(this.cuisine));
             this.$http.put('/recipe/api/cuisine/1/', JSON.stringify(this.cuisine)).then((data) => {
+                this.title = 'メッセージ';
+                this.message = 'レシピを更新しました。';
                 this.cuisine = data.body;
             }).catch((s, a, v) => {
                 console.log(s, a, v);
             });
         }
     },
-    mounted: function() {
+    mounted() {
         this.$http.get('/recipe/api/cuisine/1/').then((data) => {
             this.cuisine = data.body;
         });
---
+++ resources/js/components/Instruction.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="menu-cooking">
+        <div class="center">
+            <button type="button" class="btn-add" @click="addRow()">追加</button>
+            <button type="button" class="btn-delete" @click="deleteRow()">削除</button>
+        </div>
+        <ul class="cookings">
+            <li v-for="instruction in instructions">
+                <span class="cooking-order" v-text="getOrder(instruction)"></span>：
+                <input type="text" name="instructions.description" v-model="instruction.description" class="cooking-description">
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => {
+            return {
+            }
+        },
+        props: ['instructions'],
+        methods: {
+            addRow() {
+                this.instructions.push({});
+            },
+            deleteRow() {
+                this.instructions.pop();
+            },
+            getOrder(_instruction) {
+                if (!_instruction.sort_order) {
+                    _instruction.sort_order = this.instructions.length;
+                }
+                return _instruction.sort_order;
+            },
+        },
+    };
+</script>
