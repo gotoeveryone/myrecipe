955cffab7de4b2c20a7f79fdb47a5dbc310b3ffd
Kazuki_Kamizuru <k.amago0721@gmail.com>
2017-10-21 14:52:51 +0900

    fixes #368 アプリケーションのアセットを別ディレクトリに

--- recipe/settings.py
+++ recipe/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -134,9 +135,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'public'),
+)
 
 ASSETS_URL = os.environ.get('ASSETS_URL', '')
 
