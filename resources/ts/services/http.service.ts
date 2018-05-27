f131d9964dbcb05b10873b4261b1fda48dc1005b
Kazuki_Kamizuru <k.amago0721@gmail.com>
2018-05-27 13:49:02 +0900

    パッケージのアップデート

--- resources/ts/services/http.service.ts
+++ resources/ts/services/http.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import {
-    ConnectionBackend, Http, Request,
-    RequestOptions, RequestOptionsArgs, Response,
+    ConnectionBackend,
+    Http,
+    Request,
+    RequestOptions,
+    RequestOptionsArgs,
+    Response,
 } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { BlockUIService } from './blockui.service';
 
 /**
@@ -11,14 +16,21 @@ import { BlockUIService } from './blockui.service';
  */
 @Injectable()
 export class MyHttp extends Http {
-    constructor(backend: ConnectionBackend, defaultOptions: RequestOptions, private blockUI: BlockUIService) {
+    constructor(
+        backend: ConnectionBackend,
+        defaultOptions: RequestOptions,
+        private blockUI: BlockUIService
+    ) {
         super(backend, defaultOptions);
     }
 
-    request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+    request(
+        url: string | Request,
+        options?: RequestOptionsArgs
+    ): Observable<Response> {
         this.blockUI.block();
-        return super.request(url, options).finally(() => {
-            this.blockUI.unBlock();
-        });
+        return super
+            .request(url, options)
+            .pipe(finalize(() => this.blockUI.unBlock()));
     }
 }
