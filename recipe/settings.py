099498b595217b42bd034990f8bba7712e1ebd0d
Kazuki_Kamizuru <k.amago0721@gmail.com>
2021-03-21 20:41:42 +0900

    not use asset url

--- recipe/settings.py
+++ recipe/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'recipe.context_processors.site_common',
             ],
         },
     },
